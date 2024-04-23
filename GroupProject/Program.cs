6d7837aacc7276ff89f1d96e525e0d93cf5e98e5
AsakuraAv <vika.asakura@gmail.com>
2024-04-23 16:13:22 +0200

    Add project files.

---
+++ GroupProject/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace GroupProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello everything!");
+            Console.WriteLine("Witam w projekcie group 2 :)");
+        }
+    }
+}
