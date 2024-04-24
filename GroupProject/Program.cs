813d70f3fad343083cc147c03c9a3c6ccecf035e
Svitlana Leibak <svetlanal@devart.com>
2024-04-24 21:44:55 +0300

    Added my greeting

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -6,6 +6,8 @@
         {
             Console.WriteLine("Hello everything!");
             Console.WriteLine("Witam w projekcie group 2 :)");
+            Console.WriteLine("From Svitlana: Hi guys. Pleased to work with you."); //from Svitlana Leibak
         }
+
     }
 }
