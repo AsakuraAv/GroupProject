463ef6b757e05b26c3955654b70f77b1fe7fbe2b
Nazar Zharskyi <mrgoridon@gmail.com>
2024-04-25 17:34:42 +0300

    added multiplication table

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -6,6 +6,9 @@
         {
             Console.WriteLine("Hello everything!");
             Console.WriteLine("Witam w projekcie group 2 :)");
+            Console.WriteLine("Multiplication table: ");
+            MultipTable multip = new MultipTable();
+            multip.mulipTable();
         }
     }
 }
