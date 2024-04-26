040a79b80682e9440f4522ae0cd0f4575db4c9e2
AsakuraAv <vika.asakura@gmail.com>
2024-04-26 23:29:58 +0200

    Add star picture

--- GroupProject/Program.cs
+++ GroupProject/Program.cs
@@ -6,6 +6,20 @@
         {
             Console.WriteLine("Hello everything!");
             Console.WriteLine("Witam w projekcie group 2 :)");
+
+            StarPicture();
+        }
+
+        static void StarPicture()
+        {
+            for (int i = 1; i < 6; i++)
+            {
+                for (int j = 0; j < i; j ++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine("");
+            }
         }
     }
 }
